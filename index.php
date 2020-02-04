24a62c16b96ab443eb557598eb304c4ba01cc7b7
WilfredoHernandez <56266334+WilfredoHernandez@users.noreply.github.com>
2020-02-04 13:25:20 -0600

    First Prototype of the Page

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Questrial&display=swap" rel="stylesheet"> 
+<link rel="stylesheet" href="css/navbar.css">
+<link rel="stylesheet" href="css/footer.css">
+<link rel="stylesheet" href="css/index.css">
+<script src="script/navbar.js"></script> 
+<header>
+    <title>SE Project Prototype</title>
+</header>
+</head>
+
+<body> 
+<!-- Header Navbar Container -->
+
+    <div class="navMenu" id="navbar">
+        <div class="container">
+        <img  src="images/logo.png" width="35%" height="60%">
+        <br><br>
+            <div class="menu-sm">
+              
+            <ul id="menu">
+        <li><a href="#">Home</a></li>
+        <li>
+            <a href="#">Academics ￬</a>
+            
+            <ul class="hidden">
+            <li><a href="faculties/software.php">Software</a></li>
+                <li><a href="#">Psychology</a></li>
+                <li><a href="#">Business</a></li>
+                <li><a href="#">Management Information System</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">Research ￬</a>
+            <ul class="hidden">
+                <li><a href="#">What are they doing?</a></li>
+                <li><a href="#">Sample text</a></li>
+                <li><a href="#">Sample text</a></li>
+            </ul>
+        </li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Events & Conferences</a></li>
+        <li><a href="#">Youtube Channel</a></li>
+    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- the body starts here -->
+
+<div class="covers">
+    <img src="images/cover1.jpg" width="100%" height="100%" >
+</div>
+
+<!-- first container -->
+
+<div class="mainContent" id="main">
+    <div class="contentF">
+        <div class="contentHeader">
+        <h3 class="title">Welcome</h3>
+        </div>
+        <p></p>
+        <img src="images/container1.jpg" width="100%" height="190px" >
+    <p>Lorem ipsum dolor dummy text sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="contentS">
+    <div class="contentHeader">
+    <h3 class="title">News & Events</h3>
+    </div>
+    <p>Lorem ipsum dolor dummy text sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor dummy text sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="contentT">
+    <div class="contentHeader">
+    <h3 class="title">New Projects</h3>
+    </div>
+    <p>Lorem ipsum dolor dummy text sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor dummy text sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</div>
+</div>
+
+<!-- second container -->
+<div class="secondary">
+<br><br><br>
+  <div class="secondaryContent">
+  <img src="images/example.jpg" width="100%" height="200px">
+</div>
+<br><br><br>
+</div>
+
+</body>
+
+<!--footer -->
+<footer class="footerSite">
+  <div class="footerContent">
+      <p></p>
+      <div class="contentInfo">
+        <p></p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+
+    <p>Fill with contacts</p>
+    </div>
+
+    <div class="contentInfo">
+        <p></p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+
+    <p>Fill with contacts</p>
+    </div>
+
+    <div class="contentInfo">
+        <p></p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+
+    <p>Fill with contacts</p>
+    </div>
+
+    <div class="contentInfoR">
+        <p></p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+
+    <p>Fill with contacts</p>
+    </div>
+
+  </div>
+  
+  <div class="copyrightContent"> 
+      <p style="text-align:center; color: #999; font-family: Roboto;"> All rights reserved <span style="color:#f9b418">Keiser University Latin American Campus</span></p>
+  </div>
+</footer>
+
+</html>
---
+++ notices&event/notice.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Questrial&display=swap" rel="stylesheet"> 
+<link rel="stylesheet" href="../css/navbar.css">
+<link rel="stylesheet" href="../css/footer.css">
+<link rel="stylesheet" href="../css/notice.css">
+<script src="../script/navbar.js"></script> 
+<header>
+    <title>SE Project Prototype</title>
+</header>
+</head>
+
+<body> 
+<!-- Header Navbar Container -->
+
+    <div class="navMenu" id="navbar">
+        <div class="container">
+        <img  src="../images/logo.png" width="35%" height="60%">
+        <br><br>
+            <div class="menu-sm">
+              
+            <ul id="menu">
+        <li><a href="../index.php">Home</a></li>
+        <li>
+            <a href="#">Academics ￬</a>
+            
+            <ul class="hidden">
+                <li><a href="#">Software</a></li>
+                <li><a href="#">Business</a></li>
+                <li><a href="#">Management Information System</a></li>
+                <li><a href="#">Psychology</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">Research ￬</a>
+            <ul class="hidden">
+                <li><a href="#">What are they doing?</a></li>
+                <li><a href="#">Sample text</a></li>
+                <li><a href="#">Sample text</a></li>
+            </ul>
+        </li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Events & Conferences</a></li>
+        <li><a href="#">Youtube Channel</a></li>
+    </ul>
+                </div>
+            </div>
+        </div>
+    
+
+<!-- first container -->
+<br><br>
+<div class="mainContent" id="main">
+
+    <aside class="contentF">
+        <img src="../images/example.jpg" width="80%px" height="100%" id="vertical" >
+    </aside>
+    
+
+<div class="contentS">
+    <div class="contentHeader">
+    <h3 class="title">Here Goes the Title</h3>
+    <p> Here goes the date </p>
+    <hr>
+    </div>
+
+    <div class="contentNotice">
+    <div class="contentImage">
+        <img src="../images/example.jpg" width="100%" height="500px">
+    </div>
+    <div class="noticeInfo">
+  <p> Lorem ipsum dolor dummy text sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+  <p> Lorem ipsum dolor dummy text sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+  <p> Lorem ipsum dolor dummy text sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>   
+</div>
+
+    </div>
+    <br><br><br>
+</div>
+
+</div>
+</body>
+
+<!--footer -->
+<footer class="footerSite">
+  <div class="footerContent">
+      <br>
+      <div class="contentInfo">
+        <p></p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+
+    <p>Fill with contacts</p>
+    </div>
+
+    <div class="contentInfo">
+        <p></p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+
+    <p>Fill with contacts</p>
+    </div>
+
+    <div class="contentInfo">
+        <p></p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+
+    <p>Fill with contacts</p>
+    </div>
+
+    <div class="contentInfoR">
+        <p></p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+    <p>Fill with contacts</p>
+
+    <p>Fill with contacts</p>
+    </div>
+
+  </div>
+  
+  <div class="copyrightContent"> 
+      <p style="text-align:center; color: #999; font-family: Roboto;"> All rights reserved <span style="color:#f9b418">Keiser University Latin American Campus</span></p>
+  </div>
+</footer>
+
+</html>
